d51b96773debba50bd50ed7b45a6ee028d27d038
Nazar Kovalchuk <kovalchukny@gmail.com>
2024-08-09 09:18:34 +0200

    Add index action

--- app/controllers/api/v1/cats_controller.rb
+++ app/controllers/api/v1/cats_controller.rb
@@ -4,12 +4,7 @@ module Api
   module V1
     class CatsController < ApplicationController
       def best_deal
-        fetchers = [
-          Fetchers::HappyCatsFetcher.new,
-          Fetchers::CatsUnlimitedFetcher.new,
-        ]
-
-        service = BestCatService.new(fetchers)
+        service = build_cat_service
         best_deal = service.find_best_deal
 
         if best_deal
@@ -17,6 +12,28 @@ module Api
         else
           render json: { error: 'No cats available' }, status: :not_found
         end
+      end
+
+      def index
+        service = build_cat_service
+        all_cats = service.fetch_all_cats
+
+        if all_cats.any?
+          render json: all_cats.map(&:to_h), status: :ok
+        else
+          render json: { error: 'No cats available' }, status: :not_found
+        end
+      end
+
+      private
+
+      def build_cat_service
+        fetchers = [
+          Fetchers::HappyCatsFetcher.new,
+          Fetchers::CatsUnlimitedFetcher.new,
+        ]
+
+        CatService.new(fetchers)
       end
     end
   end
--- app/services/cat_service.rb
+++ app/services/cat_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class BestCatService
+class CatService
   def initialize(fetchers)
     @fetchers = fetchers
   end
